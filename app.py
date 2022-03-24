9872a9a988c03fc4a510f63676c470c4ee8169b8
justin-cyr <justin.rcyr@gmail.com>
2022-03-23 23:34:11 -0400

    Create react app and serve index with react bundle.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
 @app.route('/')
-def hello_world():
-    return "<p>Hello World<p>"
+def serve():
+    return send_from_directory(app.static_folder,'index.html')
